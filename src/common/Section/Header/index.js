bc4a7d482ef9948ee69ddb18407cdb0de98f6287
Kacper Lewko <k.lewkosga@gmail.com>
2024-07-10 14:37:27 +0200

    created styles for header and added folder for create Footer

--- src/common/Section/Header/index.js
+++ src/common/Section/Header/index.js
@@ -3,8 +3,8 @@ import { Container, Logo, Title } from "./styled.js";
 const Header = () => (
 
     <Container>
-        <Logo src="https://i.postimg.cc/bv5M2vg3/photo2.jpg"/>
-        <Title>Zadanie Rekrutacyjne</Title>
+        <Logo src="https://i.postimg.cc/KYdcs756/5.png"/>
+        <Title>Zadanie <strong>Rekrutacyjne</strong></Title>
 
     </Container>
     
--- src/common/Section/Header/styled.js
+++ src/common/Section/Header/styled.js
@@ -2,21 +2,29 @@ import styled from "styled-components";
 
 export const Container = styled.header`
     width: 100%;
-    height: 120px;
+    height: 100px;
     background-color: black;
     display: grid;
     grid-template-columns: 1fr 1fr;
 `
 export const Logo = styled.img`
-    border: solid;
+    background-color: #2d2d2d;
+    color: white;
+    padding: 5px;
+    //border: solid;
+    border-radius: 100%;
+    width: 60px;
     color:white;
-    margin: auto;
+    margin:  auto;
+    margin-left: 20px;
 
 `;
 
 export const Title = styled.p`
-        border: solid;
+        //border: solid;
         color:white;
+        text-align: center;
+        margin: auto;
 `;
 
 
