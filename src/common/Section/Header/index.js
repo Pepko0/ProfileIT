f98b91b858240b6f9c88aa4da7a617d547efdd8e
Kacper Lewko <k.lewkosga@gmail.com>
2024-07-14 23:31:48 +0200

    added alt and title to logo header

--- src/common/Section/Header/index.js
+++ src/common/Section/Header/index.js
@@ -6,7 +6,7 @@ const Header = ({ footerText }) => {
   return (
     <header className="header1">
       <Link to="/">
-        <img src="https://i.postimg.cc/KYdcs756/5.png" alt="Logo" className="header1__logo" />
+        <img src="https://i.postimg.cc/KYdcs756/5.png" title="Logo HTML5" alt="Logo HTML5" className="header1__logo" />
       </Link>
       <div className="header1__block-text">
         <p className="header1__title">
