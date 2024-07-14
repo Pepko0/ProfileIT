f7e3f4059feb579feb124f480a932d189967d36a
Kacper Lewko <k.lewkosga@gmail.com>
2024-07-14 23:40:13 +0200

    added title to buttons in footer and added hover to CSS IS AWESOME

--- src/common/Section/Footer/index.js
+++ src/common/Section/Footer/index.js
@@ -12,18 +12,18 @@ const Footer = ({ setFooterText, resetSettings, setSelectedOption }) => {
   const addTextOnClick = () => {
     setFooterText("Kacper Lewko");
     setButtonDisabled(true);
-    setListVisible(false); 
+    setListVisible(false);
   };
 
   const handleResetClick = () => {
     resetSettings();
     setSelectedOption(null);
     setButtonDisabled(false);
-    setListVisible(false); 
+    setListVisible(false);
   };
 
   const handleClickOutside = (event) => {
-    if (isListVisible && !event.target.closest('.footer__dropup')) {
+    if (isListVisible && !event.target.closest(".footer__dropup")) {
       setListVisible(false);
     }
   };
@@ -47,30 +47,48 @@ const Footer = ({ setFooterText, resetSettings, setSelectedOption }) => {
         </p>
       </div>
       <p className="footer__company">
-        <span className="footer__line"></span>nabthat<span className="footer__line"></span>
+        <span className="footer__line"></span>nabthat
+        <span className="footer__line"></span>
       </p>
       <div className="footer__dropup">
-        <button className="footer__button" onClick={handleButtonClick}>
+        <button
+          title="Naciśnij aby zobaczyc dodatkowe funkcje"
+          className="footer__button"
+          onClick={handleButtonClick}
+        >
           <p className="footer__button-paragraph">
             {isListVisible ? (
               <>
-                UKRYJ<span className="footer__button-paragraph-rotateDown">&gt;</span>
+                UKRYJ
+                <span className="footer__button-paragraph-rotateDown">
+                  &gt;
+                </span>
               </>
             ) : (
               <>
-                POKAŻ<span className="footer__button-paragraph-rotateUp">&gt;</span>
+                POKAŻ
+                <span className="footer__button-paragraph-rotateUp">&gt;</span>
               </>
             )}
           </p>
         </button>
-        <ul className={`footer__list ${isListVisible ? "footer__list--active" : ""}`}>
+        <ul
+          className={`footer__list ${
+            isListVisible ? "footer__list--active" : ""
+          }`}
+        >
           <li className="footer__item">
-            <button className="footer__link" onClick={handleResetClick}>
+            <button
+              title="Zresetuj Zmiany na stronie"
+              className="footer__link"
+              onClick={handleResetClick}
+            >
               &gt; ZRESETUJ USTAWIENIA
             </button>
           </li>
           <li className="footer__item">
             <button
+              title="Wyświetl w nagłówku osobę, która zrobiła Zadanie Rekrutacjne"
               className="footer__link"
               onClick={addTextOnClick}
               disabled={isButtonDisabled}
