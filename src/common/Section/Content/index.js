acfc30df155d58217b9bb33ba6afc313d1daabb1
Kacper Lewko <k.lewkosga@gmail.com>
2024-07-14 20:43:00 +0200

    added nabthat to footer

--- src/common/Section/Content/index.js
+++ src/common/Section/Content/index.js
@@ -1,153 +1,3 @@
-/*
-
-import React, { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-import "./styled.scss";
-
-const Content = ({
-  footerText,
-  setFooterText,
-  linkText,
-  selectedOption,
-  setSelectedOption,
-}) => {
-  const [texts, setTexts] = useState([]);
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    fetch(`${process.env.PUBLIC_URL}/texts.json`)
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
-        }
-        return response.json();
-      })
-      .then((data) => setTexts(data))
-      .catch((error) => console.error("Error loading texts:", error));
-  }, []);
-
-  const handleRadioChange = (option) => {
-    setSelectedOption(option);
-    if (texts.length === 0) {
-      return;
-    }
-
-    let newText = "";
-    if (option === 3) {
-      do {
-        newText = texts[Math.floor(Math.random() * texts.length)];
-      } while (newText === footerText);
-    } else {
-      newText = texts[option];
-    }
-    setFooterText(newText);
-    navigate(`/${newText}`);
-  };
-
-  const handleReplaceClick = () => {
-    if (selectedOption !== null && texts.length > 0) {
-      const newText = texts[selectedOption];
-      setFooterText(newText);
-      navigate(`/${newText}`);
-    }
-  };
-
-  const handleAppendClick = () => {
-    if (selectedOption !== null && texts.length > 0) {
-      let newText;
-      if (selectedOption === 3) {
-        do {
-          newText = texts[Math.floor(Math.random() * texts.length)];
-        } while (newText === footerText);
-      } else {
-        newText = texts[selectedOption];
-      }
-      setFooterText((prevText) => {
-        const updatedFooterText = [...prevText.split(" "), newText]
-          .sort()
-          .join(" ");
-        navigate(`/${updatedFooterText}`);
-        return updatedFooterText;
-      });
-    }
-  };
-
-  return (
-    <div className="container">
-      <header className="header">Nagłówek H1</header>
-      <div className="blocks">
-        
-        
-        <div className="block-left">
-          <h2 className="title">BLOK PIERWSZY</h2>
-          <label className="label">
-            <input
-              type="radio"
-              name="option"
-              className="radio-button"
-              checked={selectedOption === 0}
-              onChange={() => handleRadioChange(0)}
-            /><i className="check"></i>
-            <span className="check_text">Opcja pierwsza</span>
-          </label>
-          <label className="label">
-            <input
-              type="radio"
-              name="option"
-              className="radio-button"
-              checked={selectedOption === 1}
-              onChange={() => handleRadioChange(1)}
-            /><i className="check"></i>
-            <span className="check_text">Opcja druga</span>
-          </label>
-          <label className="label">
-            <input
-              type="radio"
-              name="option"
-              className="radio-button"
-              checked={selectedOption === 3}
-              onChange={() => handleRadioChange(3)}
-            /><i className="check"></i>
-            <span className="check_text">Opcja Trzecia</span>
-          </label>
-        </div>
-        
-        
-        
-        <div className="block-right">
-          <h2 className="title">BLOK DRUGI</h2>
-          <div className="block-more-right">
-            <label className="label">
-              <button className="button" onClick={handleReplaceClick}>
-                <p className="button-paragraph">ZASTĄP</p>
-              </button>
-            </label>
-            <label className="label">
-              <button className="button" onClick={handleAppendClick}>
-                <p className="button-paragraph">DOKLEJ</p>
-              </button>
-            </label>
-          </div>
-        </div>
-        <div className="block-footer">
-          <h2 className="title">BLOK Z DŁUGĄ NAZWĄ KTÓRA SAMA SIĘ PRZYTNIE ...</h2>
-          <p className="footer-text">{footerText}</p>
-          <a href={`/${footerText}`}>{linkText}</a>
-        </div>
-      </div>
-      <div className="footer1">
-        <h2 className="title">BLOK Z DŁUGĄ NAZWĄ KTÓRA SAMA SIĘ PRZYTNIE ...</h2>
-        <p className="footer-text">{footerText}</p>
-        <a href={`/${footerText}`}>{linkText}</a>
-      </div>
-    </div>
-  );
-};
-
-export default Content;
-
-*/
-
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import "./styled.scss";
@@ -171,9 +21,9 @@ const Content = ({
         return response.json();
       })
       .then((data) => {
-        setTexts(data.texts || []); // Pobieranie tekstów do opcji radiowych
+        setTexts(data.texts || []);
         if (data.initialText) {
-          setFooterText(data.initialText); // Ustawienie początkowego tekstu
+          setFooterText(data.initialText);
         }
       })
       .catch((error) => console.error("Error loading texts:", error));
@@ -278,7 +128,7 @@ const Content = ({
         </div>
       </div>
       <div className="footer1">
-        <h2 className="title">BLOK Z DŁUGĄ NAZWĄ KTÓRA SAMA SIĘ PRZYTNIE ...</h2>
+        <h2 className="title_cut">BLOK Z DŁUGĄ NAZWĄ KTÓRA SAMA SIĘ PRZYTNIE ...</h2>
         <p className="footer-text">{footerText}</p>
         <a href={`/${footerText}`}>{linkText}</a>
       </div>
--- src/common/Section/Footer/index.js
+++ src/common/Section/Footer/index.js
@@ -1,3 +1,4 @@
+
 import React, { useState } from "react";
 import "./styled.scss";
 
@@ -23,76 +24,21 @@ const Footer = ({ setFooterText, resetSettings, setSelectedOption }) => {
   return (
     <footer>
       <div className="square">
-        <p className="square-text">CSS<br/>IS<br/>AWESOME</p>
+        <p className="square-text">
+          CSS
+          <br />
+          IS
+          <br />
+          AWESOME
+        </p>
       </div>
       <p className="company">
-        <svg
-          width="20"
-          height="20"
-          fill="none"
-          stroke="white"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth="3"
-          viewBox="0 0 24 24"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path d="M1.5 12h21"></path>
-        </svg>
-        nabthat
-        <svg
-          width="20"
-          height="20"
-          margin="auto"
-          fill="none"
-          stroke="white"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth="3"
-          viewBox="0 0 24 24"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path d="M1.5 12h21"></path>
-        </svg>
+      <span className="line-only"></span>nabthat<span className="line-only"></span>
       </p>
       <div className="dropup">
         <button className="button1" onClick={handleButtonClick}>
           <p className="button-paragraph">
-            {isListVisible ? (
-              <>
-                UKRYJ
-                <svg
-                  width="20"
-                  height="20"
-                  fill="none"
-                  stroke="gray"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="3"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path d="m6 9 6 6 6-6"></path>
-                </svg>
-              </>
-            ) : (
-              <>
-                POKAŻ
-                <svg
-                  width="20"
-                  height="20"
-                  fill="none"
-                  stroke="gray"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="3"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path d="m18 15-6-6-6 6"></path>
-                </svg>
-              </>
-            )}
+            {isListVisible ? <>UKRYJ<span className="button_paragraph-rotateDown" >&gt;</span></> : <>POKAŻ  <span className="button_paragraph-rotateUp" >&gt;</span></>}
           </p>
         </button>
         <ul className={`lists ${isListVisible ? "active" : ""}`}>
